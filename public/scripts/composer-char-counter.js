2c8329609634d91e51299c28d87c289fb0cd6f3c
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-18 19:23:13 +0000

    no change

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 
 
-  $('#tweet-text').keyup(function() {
+  $('#tweet-text').keypress(function() {
     let max = 140;
     let length = max - ($(this).val().length);
     //$('.counter').text(length);
-    let sib = $(this).parent().find("output.counter")
-    sib.text(length)
+    let tweet = $(this).parent().find("output.counter")
+    tweet.text(length)
 
     if (length <= 0) {
-      sib.addClass('red')
+      tweet.addClass('red')
     } else {
-      sib.removeClass('red')
+      tweet.removeClass('red')
     };
   })
 });
