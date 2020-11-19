88fab9243aec42517cbb2f415935af0b9fa10835
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-19 02:52:04 +0000

    loadTweet func Ajax working error msg start

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,62 +5,86 @@
  */
 
 $(document).ready(function() {
-
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
+  
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
   }
-]
 
-const renderTweets = function(tweets) {
-  for (let tweet of tweets) {
-    const newTweet = createTweetElement(tweet)
-    $('#tweet-container').append(newTweet)
+  // array with all the tweet - generate tweet HTML
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      const newTweet = createTweetElement(tweet)
+      $('#tweet-container').prepend(newTweet)
+    }
+  };
+  
+  // return HTML for rendering the tweet
+  const createTweetElement = function(tweet) {
+    let $tweet = `
+    <article class="tweet">
+    <header>
+      <img src=${tweet.user.avatars} alt="avatar" style="width:40px;height:40px;">
+      <h2>${tweet.user.name}</h2>
+      <h3>${tweet.user.handle}</h3>
+    </header>
+    <p>${escape(tweet.content.text)}</p>
+    <footer>
+      <span>
+        ${tweet.created_at}
+      </span>
+      <div>
+        <img src="/images/repost.png" alt="repost" style="width:20px;height:20px;">
+        <img src="/images/heart.png" alt="heart" style="width:20px;height:20px;">
+        <img src="/images/little-flag.svg" alt="heart" style="width:20px;height:20px;">
+      </div>
+    </footer>
+  </article>`
+    return $tweet;
   }
-};
-
-const createTweetElement = function(tweet) {
-  let $tweet = `
-  <article class="tweet">
-  <header>
-    <img src=${tweet.user.avatars} alt="avatar" style="width:40px;height:40px;">
-    <h2>${tweet.user.name}</h2>
-    <h3>${tweet.user.handle}</h3>
-  </header>
-  <p>${tweet.content.text}</p>
-  <footer>
-    <span>
-      ${tweet.created_at}
-    </span>
-    <div>
-      <img src="/images/repost.png" alt="repost" style="width:20px;height:20px;">
-      <img src="/images/heart.png" alt="heart" style="width:20px;height:20px;">
-      <img src="/images/little-flag.svg" alt="heart" style="width:20px;height:20px;">
-    </div>
-  </footer>
-</article>`
-  return $tweet;
-}
-
-renderTweets(data);
+  
+  
+  //form sumbission
+  $('form').on('submit', event => {
+    event.preventDefault();
+    
+    if (!$('#tweet-text').val()) {
+      $('.error-msg').slideDown();
+      $('.error-msg').text('Your tweet is empty!')
+    } else if ($('#tweet-text').val().length > 140) {
+      $('.error-msg').slideDown();
+      $('.error-msg').text('Your tweet is over character capacity');
+    } else {
+      $.ajax({
+        url:"/tweets",
+        method:"POST",
+        data: $('form').serialize(),
+      })
+      .then(function() {
+        $('#tweet-container').empty()
+        loadTweets()
+      })
+      .catch((err) => console.log(err));
+    }
+  })
+ 
+  
+  //load tweet on the page without refreshing 
+  const loadTweets = () => {
+    $.ajax({
+      url: "/tweets",
+      method: "GET"
+    })
+    .then(function(res) {
+      renderTweets(res)
+    });
+  };
 
 })
+
+
+
+
+
+
