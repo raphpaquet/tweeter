d3d5ef10a08f212e7975eafe94db40d725e1aaf6
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-19 02:52:24 +0000

    back at keyup

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
 
 
-  $('#tweet-text').keypress(function() {
+  $('#tweet-text').keyup(function() {
     let max = 140;
     let length = max - ($(this).val().length);
-    //$('.counter').text(length);
     let tweet = $(this).parent().find("output.counter")
     tweet.text(length)
 
